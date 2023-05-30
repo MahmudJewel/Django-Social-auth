248c2acd19951963105cf7cb85e2a2fea544c705
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-05-30 20:38:50 +0600

    user creation method done

--- authmanagement/views.py
+++ authmanagement/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -7,19 +9,6 @@ from .forms import RegisterForm
 class RegisterView(CreateView):
     template_name = 'auth/signup.html'
     form_class = RegisterForm
-    success_url = reverse_lazy('/')
-
-
-# class RegisterView():
-#     def post():
-#         register_form = RegisterForm()
-
-# def createUser(request):
-#     template_name = 'auth/signup.html'
-#     register_form = RegisterForm()
-#     context = {
-#         'register_form': register_form,
-#     }
-#     return render(request, template_name, context)
+    success_url = reverse_lazy('login')
 
 
