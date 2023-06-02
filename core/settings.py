245330aabccb27c1cf6007ee090636df290b8785
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-06-02 18:18:59 +0600

    Linkedin set up done

--- core/settings.py
+++ core/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
 
     'allauth.socialaccount.providers.google',
-    # 'allauth.socialaccount.providers.linkedin',
+    'allauth.socialaccount.providers.linkedin_oauth2',
     # end all auth
 ]
 # all auth
