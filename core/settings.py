b42fd61f14c6b8ff8b3d93c0dd4e0f2d2108cdab
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-06-05 19:59:44 +0600

    password reset done

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -204,5 +205,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'your_account@gmail.com'
-EMAIL_HOST_PASSWORD = "your account's password"
+EMAIL_HOST_USER = config('EMAIL')
+EMAIL_HOST_PASSWORD = config('PASS')
+# print('=========>', config('EMAIL'), config('PASS'))
+# ydhthkzfbfiwmqrl
